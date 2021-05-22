64731907b50532a66e1bc2aee14aeef8ead8c874
Joel Peyton <joelpeyton@hotmail.co.uk>
2021-05-22 14:48:12 +0100

    Fixed bug

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
     hideDelivery();
     hideAbout();
     hideSelling();
+    hideJoel();
     toggleLoader("hide");
     landingPage();
     //getInfo(sessionStorage.initialCategory);
