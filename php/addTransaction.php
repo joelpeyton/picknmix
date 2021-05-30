eb8415f1894528e965e16f56bb9a85a85c8df855
Joel Peyton <joelpeyton@hotmail.co.uk>
2021-05-30 11:35:14 +0100

    fix bugs

--- php/addTransaction.php
+++ php/addTransaction.php
@@ -14,6 +14,8 @@
     $transaction = new Transaction($db);
     $transaction->transaction_id = htmlspecialchars(strip_tags($_GET["transaction_id"]));
     $transaction->total = htmlspecialchars(strip_tags($_GET["total"]));
+    $transaction->discount = htmlspecialchars(strip_tags($_GET["discount"]));
+    $transaction->promo_discount = htmlspecialchars(strip_tags($_GET["promo_discount"]));
     $transaction->shipping = htmlspecialchars(strip_tags($_GET["shipping"]));
     $transaction->grand_total = htmlspecialchars(strip_tags($_GET["grand_total"]));
     $transaction->customer_id = $customer->id;
