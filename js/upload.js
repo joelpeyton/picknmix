fa0d5eb3530fdcdb71be5d76a2cca37757c5accc
Joel Peyton <joelpeyton@hotmail.co.uk>
2021-06-13 10:40:09 +0100

    minor tweeks

--- js/upload.js
+++ js/upload.js
@@ -1,10 +1,43 @@
 "use strict";
 
 $(document).ready(function() {
-    $("#check").click(function(){
-        let from = document.getElementById("from");
-        let to = document.getElementById("to");
+    let from = document.getElementById("from");
+    let to = document.getElementById("to");
+    let options = document.getElementsByTagName("option");
 
+    for (let i = 2; i < 10; i++) {
+        options[i].hidden = false;
+    }
+
+    for (let i = 10; i < 13; i++) {
+        options[i].hidden = true;
+    }
+
+    $("#from").change(function() {
+        if (from.value == "fixed") {
+            options[2].selected = true;
+            for (let i = 2; i < 10; i++) {
+                options[i].hidden = false;
+            }
+    
+            for (let i = 10; i < 13; i++) {
+                options[i].hidden = true;
+            }
+        }
+    
+        else {
+            options[10].selected = true;
+            for (let i = 2; i < 10; i++) {
+                options[i].hidden = true;
+            }
+    
+            for (let i = 10; i < 13; i++) {
+                options[i].hidden = false;
+            }
+        }
+    });  
+
+    $("#check").click(function(){
         $.ajax({
             type: "GET",
             url: "php/upload.php?table=" + from.value + "&to=" + to.value
